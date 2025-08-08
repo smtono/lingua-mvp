9b0db4f2e5fcc8eca39dde1df02ae808df10e0c6
tono || トノ <64560893+smtono@users.noreply.github.com>
2025-08-08 10:10:53 -0500

    Create User Preferences template

---
+++ src/user/user_preferences.py
@@ -0,0 +1,22 @@
+"""
+
+"""
+
+
+class UserPreferences:
+    """
+    e
+    """
+
+    def __init__(self):
+        pass
+
+    def update_preferences(self):
+        """
+        Fetches User data from the DB and updates accordingly
+        """
+
+    def get_preferences(self, user):
+        """
+        Fetches user data to return back to user
+        """
