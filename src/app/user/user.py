65a887730f2d5b93c1b54b17e6cd485c60b613b5
tono || トノ <64560893+smtono@users.noreply.github.com>
2025-08-21 10:29:23 -0500

    Update user classes with appropriate docstrings

--- src/app/user/user.py
+++ src/app/user/user.py
@@ -1,44 +1,41 @@
 """
 Module for managing user profiles and tracking performance for a
-language learning or quiz-based application.
+language learning or quiz-based application
 
 Classes:
     User:
         Represents an individual engaging with the program,
             including their Discord context,
-        language preferences, proficiency level, and topics of interest.
+        language preferences, proficiency level, and topics of interest
         This information is used to fine-tune quiz content
-            and learning recommendations.
+            and learning recommendations
 
     PerformanceTracker:
-        Handles tracking user performance on quizzes and activities.
+        Handles tracking user performance on quizzes and activities
         Updates user profiles, manages level progression,
             and stores relevant metrics
-        to personalize future quizzes and monitor learning outcomes.
+        to personalize future quizzes and monitor learning outcomes
 """
 
 
+from dataclasses import dataclass
+
+
+@dataclass
 class User:
     """
     Represents a user interacting through Discord, including their
-    language preference, proficiency level, and preferred topics.
+    language preference, proficiency level, and preferred topics
+
+    Attributes:
+        user_ctx: User context to provide when
+            generating quizzes, updating preferences, and so on
+        languages: Primary languages user is learning
+        level: Current level of user
+        topics: User input topics to use when generating quizzes
     """
 
-    def __init__(self,
-                 user_ctx: dict,
-                 language: str,
-                 level: int,
-                 topics: list[str]):
-        """
-        Initializes a User instance.
-
-        Args:
-            user_ctx (dict): Metadata for the user.
-            language (str): The language the user is interested in or uses.
-            level (int): The user's language proficiency level.
-            topics (list[str]): List of topics the user prefers.
-        """
-        self.user_ctx = user_ctx
-        self.language = language
-        self.level = level
-        self.topics = topics
+    user_ctx: dict
+    languages: list[str]
+    level: int
+    topics: list[str]
--- src/app/user/user_preferences.py
+++ src/app/user/user_preferences.py
@@ -3,21 +3,26 @@ Updates to user preferences interacting with the database
 """
 
 
+from app.user.sql.database import Database
+from app.user.user import User
+
+
 class UserPreferences:
     """
     Changes attributes of User's preferences
     based on passed in User object and commands
+
+    Attributes:
+        user: User object to edit preferences of in the database
+        cursor: Database object to execute queries with
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, user: User, cursor: Database):
+        self.user = user
+        self.cursor = cursor
 
     def update_preferences(self):
-        """
-        Fetches User data from the DB and updates accordingly
-        """
+        """Fetches User data from the DB and updates accordingly"""
 
     def get_preferences(self, user):
-        """
-        Fetches user data to return back to user
-        """
+        """Fetches user data to return back to user"""
