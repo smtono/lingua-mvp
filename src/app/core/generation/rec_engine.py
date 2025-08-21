8ba008cad49e2c07f89a08919e48c67692806b73
tono || トノ <64560893+smtono@users.noreply.github.com>
2025-08-20 21:14:37 -0500

    Fix linter whining

--- src/app/core/generation/rec_engine.py
+++ src/app/core/generation/rec_engine.py
@@ -5,10 +5,11 @@ Validate user input and provide feedback
 Has a fact checking component to it
 """
 
+
 class RecommendationEngine:
     """
     Updates user's data based on passed arguments on recommendations
-    
+
     Attributes:
         user: User object to update in database
         cursor: Database object to update with
@@ -17,8 +18,11 @@ class RecommendationEngine:
     def __init__(self, user, cursor):
         self.user = user
         self.cursor = cursor
-    
+
     def update_on(self, args):
         """
         Update on args of args
         """
+
+    def give(self):
+        """Placeholder for giving output of engine"""
