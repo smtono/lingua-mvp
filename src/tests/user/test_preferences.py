33a667c56e81ee2d59cbd136cd37f3077a0359ef
tono || トノ <64560893+smtono@users.noreply.github.com>
2025-08-28 12:44:55 -0500

    Add init files to initialize python modules in app, add to basic cases for changing user preferences, created dummy user with generic preferences, etc

--- src/tests/user/test_preferences.py
+++ src/tests/user/test_preferences.py
@@ -3,6 +3,9 @@ Test user preferences changing through CLI execution
 """
 
 from unittest import TestCase
+from app.user.user_preferences import UserPreferences
+from app.user.user import User
+from app.user.sql.database import Database
 
 
 class TestUserPreferences(TestCase):
@@ -10,14 +13,31 @@ class TestUserPreferences(TestCase):
     Test User preferences
     """
 
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.test_database = Database("test.db")
+        self.dummy_user = User(
+            {"static_key": "variable_value"}, ["en", "jp"], 2, ["food"]
+        )
+
     ##############################
     #   BASE CASE
     ##############################
     def test_update_context(self):
         """Updates user_ctx"""
+        preferences = UserPreferences(self.dummy_user, self.test_database)
+        preferences.update_preferences()
+
+        # Assert context changed
+        # TODO
 
     def test_update_languages(self):
         """Updates user languages"""
+        languages = self.dummy_user.languages
+        #self.dummy_user.
+
+        # Assert context changed
+        # TODO
 
     def test_update_level(self):
         """Updates user level"""
