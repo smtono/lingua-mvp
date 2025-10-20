0bf6c0b4d2ae18fe1073a9a126156163084541f2
tono || トノ <64560893+smtono@users.noreply.github.com>
2025-10-20 11:50:28 -0500

    Add skeleton for database unit tests

--- src/tests/db/test_db.py
+++ src/tests/db/test_db.py
@@ -4,3 +4,21 @@ Test database interactions
 
 from unittest import TestCase
 from app.sql.database import Database
+
+class TestDatabase(TestCase):
+    """
+    Test Database functionality
+    """
+
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.test_database = Database("test", "dummy.db")
+
+
+    def test_database_exists():
+        """
+        """
+
+    def test_database_nonexistent():
+        """
+        """
